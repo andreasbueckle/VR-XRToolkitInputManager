a4b3f24334dce586164ef9396346bf1100fcd111
Andreas Bueckle <abueckle@iu.edu>
2022-08-11 18:52:26 -0400

    Updated to 2021.3, followed tutorial, added secondary button input

--- Assets/_Input/Scripts/ExampleListener.cs
+++ Assets/_Input/Scripts/ExampleListener.cs
@@ -3,33 +3,79 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ExampleListener : MonoBehaviour
 {
+    public ButtonHandler PrimaryButtonPressHandler = null;
+    public ButtonHandler SecondaryButtonPressHandler = null;
+    public ButtonHandler PrimaryAxisClickHandler = null;
+    public AxisHandler2D PrimaryAxisHandler = null;
+    public AxisHandler triggerHandler = null;
+
     public void OnEnable()
     {
+        PrimaryAxisClickHandler.OnButtonDown += PrintTouchButtonDown;
+        PrimaryAxisClickHandler.OnButtonUp += PrintTouchButtonUp;
 
+        PrimaryAxisHandler.OnValueChange += PrintPrimaryAxis;
+        triggerHandler.OnValueChange += PrintTrigger;
+
+        PrimaryButtonPressHandler.OnButtonDown += PrintPrimaryButtonDown;
+        PrimaryButtonPressHandler.OnButtonUp += PrintPrimaryButtonUp;
+
+        SecondaryButtonPressHandler.OnButtonDown += PrintSecondaryButtonDown;
+        SecondaryButtonPressHandler.OnButtonUp += PrintSecondaryButtonUp;
     }
 
     public void OnDisable()
     {
+        PrimaryAxisClickHandler.OnButtonDown -= PrintTouchButtonDown;
+        PrimaryAxisClickHandler.OnButtonUp -= PrintTouchButtonUp;
+        
+        PrimaryAxisHandler.OnValueChange -= PrintPrimaryAxis;
+        triggerHandler.OnValueChange -= PrintTrigger;
 
+        PrimaryButtonPressHandler.OnButtonDown -= PrintPrimaryButtonDown;
+        PrimaryButtonPressHandler.OnButtonUp -= PrintPrimaryButtonUp;
+
+        SecondaryButtonPressHandler.OnButtonDown -= PrintSecondaryButtonDown;
+        SecondaryButtonPressHandler.OnButtonUp -= PrintPrimaryButtonUp;
+    }
+
+    private void PrintTouchButtonDown(XRController controller)
+    {
+        print("touch button down");
+    }
+
+    private void PrintTouchButtonUp(XRController controller)
+    {
+        print("touch button up");
     }
 
     private void PrintPrimaryButtonDown(XRController controller)
     {
-
+        print("primary button down");
     }
 
     private void PrintPrimaryButtonUp(XRController controller)
     {
+        print("primary button up");
+    }
 
+    private void PrintSecondaryButtonDown(XRController controller)
+    {
+        print("secondary button down");
+    }
+
+    private void PrintSecondaryButtonUp(XRController controller)
+    {
+        print("secondary button up");
     }
 
     private void PrintPrimaryAxis(XRController controller, Vector2 value)
     {
-
+        print("Joystick moved :" + value);
     }
 
     private void PrintTrigger(XRController controller, float value)
     {
-
+        print("Trigger pressed: " + value);
     }
 }
--- Assets/_Input/Scripts/InputManager.cs
+++ Assets/_Input/Scripts/InputManager.cs
@@ -4,29 +4,46 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class InputManager : MonoBehaviour
 {
+    public List<ButtonHandler> allButtonHandlers = new List<ButtonHandler>();
+    public List<AxisHandler2D> allAxisHandlers2D = new List<AxisHandler2D>();
+    public List<AxisHandler> allAxisHandlers = new List<AxisHandler>();
+
+    private XRController controller = null;
+
     private void Awake()
     {
-
+        controller = GetComponent<XRController>();
     }
 
     private void Update()
     {
-
+        HandleButtonEvents();
+        HandleAxisEvents();
+        HandleAxis2DEvents();
     }
 
     private void HandleButtonEvents()
     {
-
+        foreach (ButtonHandler Handler in allButtonHandlers)
+        {
+            Handler.HandleState(controller);
+        }
     }
 
     private void HandleAxis2DEvents()
     {
-
+        foreach (AxisHandler handler in allAxisHandlers)
+        {
+            handler.HandleState(controller);
+        }
     }
 
     public void HandleAxisEvents()
     {
-
+        foreach (AxisHandler2D handler in allAxisHandlers2D)
+        {
+            handler.HandleState(controller);
+        }
     }
 }
 
